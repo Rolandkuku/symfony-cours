9300d0b5f125e531bf42a821e02217bf6b344fd3
Roland Kullet <renaud.bellec.3@gmail.com>
2017-11-02 10:01:58 +0100

    Article crud

--- src/AppBundle/Controller/ArticleController.php
+++ src/AppBundle/Controller/ArticleController.php
@@ -46,12 +46,78 @@ class ArticleController extends Controller
 
             $this->addFlash(`success`, `L'article {$article->getTitle()} a été créé!`);
 
-            return $this->redirectToRoute('admin_article_index');
+            return $this->redirectToRoute('article_show', ["id" => $article->getId()]);
         }
 
         return $this->render('article/create.html.twig', [
             'article' => $article,
             'form' => $form->createView()
         ]);
+    }
+
+     /**
+     *  Updates an article
+     *  @Route("/{id}/update", name="article_update")
+     *  @Method({"GET", "POST"})
+     *  @return void
+     */
+    public function updateAction(Request $request, Article $article) {
+        $em = $this->getDoctrine()->getManager();
+
+        $article = $em->getRepository("AppBundle:Article")->find($article);
+        $form = $this->createForm(ArticleType::class, $article);
+
+        $form->handleRequest($request);
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->flush();
+
+            $this->addFlash(`success`, `L'article {$article->getTitle()} a été modifié !`);
+
+            return $this->redirectToRoute('article_show', ["id" => $article->getId()]);
+        }
+
+        return $this->render('article/create.html.twig', [
+            'article' => $article,
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * Shows an article
+     * @Route("/{id}", name="article_show")
+     * @Method({"GET"})
+     * @param Article $article
+     * @return response
+     */
+    public function showAction(Request $req, Article $article) {
+        $em = $this->getDoctrine()->getManager();
+        $article = $em->getRepository("AppBundle:Article")->find($article);
+
+        return $this->render("article/show.html.twig", [
+            "article" => $article
+        ]);
+    }
+
+    /**
+     * Deletes an article
+     * @Route("/delete/{id}/token", name="article_delete")
+     * @Method({"GET"})
+     * @param Request $request
+     * @param Article $article
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     */
+    public function deleteAction(Request $request, Article $article) {
+        $token = $request->attributes->get("token");
+
+        if($this->isCsrfTokenValid("delete_article", $token)) {
+            throw new AccessDeniedException("Erreur CSRF");
+        }
+
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($article);
+        $em->flush();
+
+        return $this->redirectToRoute("article_index");
     }
 }
