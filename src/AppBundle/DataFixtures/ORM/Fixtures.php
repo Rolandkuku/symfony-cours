5753bc7c8c47d80462dafe541fb1e5868a02c7f3
Roland Kullet <renaud.bellec.3@gmail.com>
2017-11-03 09:54:57 +0100

    Used fixtures

--- src/AppBundle/DataFixtures/ORM/Fixtures.php
+++ src/AppBundle/DataFixtures/ORM/Fixtures.php
@@ -3,18 +3,37 @@
 namespace AppBundle\DataFixtures\ORM;
 
 use AppBundle\Entity\Tag;
+use AppBundle\Entity\Article;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+
 
 class Fixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // create 20 products! Bam!
+        $faker = \Faker\Factory::create();
+        $tags = [];
         for ($i = 0; $i < 20; $i++) {
             $tag = new Tag();
-            $tag->setName('tag '.$i);
+            $tag->setName($faker->name);
+            $tags[] = $tag;
             $manager->persist($tag);
+        }
+
+        for ($i = 0; $i < 20; $i++) {
+            $article = new Article();
+            $article->setTitle($faker->name);
+            $article->setContent($faker->text);
+            $articleTags = [];
+            for($i2 = 0; $i2 < 5; $i2++) {
+                $tag = $tags[rand(0, 19)];
+                if (!in_array($tag, $articleTags)) {
+                    $articleTags[] = $tag;
+                    $article->addTag($tag);
+                }
+            }
+            $manager->persist($article);
         }
 
         $manager->flush();
